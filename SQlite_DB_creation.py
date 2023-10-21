8ac369cc629a6b761201e91ef1559a218786c99d
oliverkisza <64221165+oliverkisza@users.noreply.github.com>
2023-10-21 15:59:41 -0700

    Updated SQlite_DB_creation.py

--- SQlite_DB_creation.py
+++ SQlite_DB_creation.py
@@ -1,36 +1,46 @@
 import sqlite3
 import csv
 
-#make sqlite db + establish connection
-conn = sqlite3.connect('proj03_database.db')
+conn = sqlite3.connect("weather.db")
 cursor = conn.cursor()
 
-#make question_3 table
+#check if table exists, drop if yes
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='weather_data'")
+table_exists = cursor.fetchone()
+
+if table_exists:
+    cursor.execute("DROP TABLE weather_data")
+
+#make table to import weatherdata csv into
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS question_3 (
+    CREATE TABLE weather_data (
         country TEXT,
         location_name TEXT,
         latitude REAL,
         longitude REAL,
+        temperature_fahrenheit REAL,
         condition_text TEXT,
-        precip_mm REAL
-)
+        wind_mph REAL,
+        precip_in REAL,
+        humidity INTEGER,
+        cloud INTEGER,
+        visibility_miles REAL,
+        uv_index INTEGER,
+        air_quality_PM2_5 INTEGER,
+        air_quality_PM10 INTEGER,
+        air_quality_us_epa_index INTEGER,
+        moon_phase TEXT,
+        moon_illumination INTEGER
+    )
 ''')
 
-
-
-
-#read data from csv and import into question_3 table
-with open('GlobalWeatherRepository-Oliver.csv', 'r') as csv_file:
+#read csv and import weatherdata into table
+#MAKE SURE TO CHANGE PATH for github repo, currently using personal path prior to update - WONT work otherwise!!
+with open('Oliver_work\data\GlobalWeatherRepository_full_data.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
-
     for row in csv_reader:
-        cursor.execute('''
-            INSERT INTO question_3 (country, location_name, latitude, longitude, condition_text, precip_mm)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ''', (row[0], row[1], float(row[2]), float(row[3]), row[4], float(row[5])))
+        cursor.execute("INSERT INTO weather_data VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                       (row[0], row[1], row[2], row[3], row[8], row[9], row[10], row[16], row[17], row[23], row[24], row[27], row[28], row[29], row[30], row[36]))
 
-#commit changes and close db connection
 conn.commit()
-conn.close()
