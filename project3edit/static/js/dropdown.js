7881fb62d88f293a69714e0bab5b601df59e1452
DarrickM <136608998+DarrickM@users.noreply.github.com>
2023-10-24 15:04:06 -0700

    Add files via upload

---
+++ project3edit/static/js/dropdown.js
@@ -0,0 +1,54 @@
+
+function init() {
+       
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+    
+    // Use D3 to get sample names and populate the drop-down selector
+    
+        // Set a variable for the sample names
+        let questions = [];
+        questions.push("Select Visualization");
+        questions.push("How does air quality differ by location?");
+        questions.push("How do latitude affect temperature and wind speed?");
+        questions.push("Do latitude and longitude affect precipitation frequency and magnitude?");
+        questions.push("Does air quality affect levels of moon illumination?");
+        console.log(questions)
+        questions.forEach(x => {dropdownMenu.append("option").text(x)
+        });
+    }
+        init()
+
+function buildScatterplot(temperature) {
+console.log(data.latitude)
+    latitude = Object.values(data.latitude);
+	longitude = Object.values(data.longitude);
+	temp = Object.values(data.temperature_fahrenheit);
+                let scatter= {
+                    x: latitude,
+                    y: temp,
+                    mode: "markers",
+                    type: "scatter",
+                    marker: {size:12}
+        
+                };
+                //set up the chart layout
+                let layout = {
+                    title: "lattitude vs temperature"
+                    
+                };
+        
+                // Call Plotly to plot the bar chart
+                Plotly.newPlot("scatter", [scatter], layout);
+        
+            }
+    document.querySelector("#selDataset").onchange = function(){
+        if (event.target.options.selectedIndex == 0) {
+            document.querySelector("#scatter").innerHTML=""
+        } else if (event.target.options.selectedIndex == 1) {
+            createMap();
+        } else if (event.target.options.selectedIndex == 2) {
+            buildScatterplot()
+        } 
+        
+    }
